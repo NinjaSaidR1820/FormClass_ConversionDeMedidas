f24ed6b022a62fc834a9c47c2ea1c55b13c72351
EngellJuarez <juarezengell8@gmail.com>
2022-01-18 10:37:53 -0600

    Agregar archivos de proyecto.

---
+++ FormClass/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace FormClass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtMetros = new System.Windows.Forms.TextBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnClean = new System.Windows.Forms.Button();
+            this.lstR = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(432, 205);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(132, 32);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calcular";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(265, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 34);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Control de Medidas";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft JhengHei UI", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(54, 212);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ingrese Valor en metros";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(158, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(424, 123);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtMetros
+            // 
+            this.txtMetros.Location = new System.Drawing.Point(244, 211);
+            this.txtMetros.Name = "txtMetros";
+            this.txtMetros.Size = new System.Drawing.Size(172, 20);
+            this.txtMetros.TabIndex = 4;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalir.Location = new System.Drawing.Point(138, 25);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(132, 32);
+            this.btnSalir.TabIndex = 5;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnClean
+            // 
+            this.btnClean.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClean.Location = new System.Drawing.Point(0, 25);
+            this.btnClean.Name = "btnClean";
+            this.btnClean.Size = new System.Drawing.Size(132, 32);
+            this.btnClean.TabIndex = 6;
+            this.btnClean.Text = "Limpiar";
+            this.btnClean.UseVisualStyleBackColor = true;
+            this.btnClean.Click += new System.EventHandler(this.btnClean_Click);
+            // 
+            // lstR
+            // 
+            this.lstR.FormattingEnabled = true;
+            this.lstR.Location = new System.Drawing.Point(158, 252);
+            this.lstR.Name = "lstR";
+            this.lstR.Size = new System.Drawing.Size(424, 108);
+            this.lstR.TabIndex = 7;
+            this.lstR.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnClean);
+            this.groupBox1.Controls.Add(this.btnSalir);
+            this.groupBox1.Location = new System.Drawing.Point(216, 366);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(296, 72);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Actions";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lstR);
+            this.Controls.Add(this.txtMetros);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Form1";
+            this.Text = "Control de Medidas";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtMetros;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnClean;
+        private System.Windows.Forms.ListBox lstR;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ FormClass/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormClass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double metros = double.Parse(txtMetros.Text);
+
+            double centimetros = metros * 100;
+            double pulgadas = centimetros / 2.54;
+            double pies = pulgadas / 12;
+            double yardas = pulgadas / 3;
+                 
+            lstR.Items.Add(" |||||| Resumen de Conversiones |||||||");
+            lstR.Items.Add("---------------------------------------------");
+            lstR.Items.Add("Medida en Metros: "+metros.ToString("0.00"));
+            lstR.Items.Add("Medida en Centimetros: "+centimetros.ToString("0.00"));
+            lstR.Items.Add("Medida en Pulgadas: " + pulgadas.ToString("0.00"));
+            lstR.Items.Add("Medida en Pies: " + pies.ToString("0.00"));
+            lstR.Items.Add("Medida en Yardas: " + yardas.ToString("0.00"));
+
+            lstR.Items.Add("");
+        }
+
+        private void btnClean_Click(object sender, EventArgs e)
+        {
+            txtMetros.Clear();
+            lstR.Items.Clear();
+            txtMetros.Focus();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+
+            DialogResult r = MessageBox.Show("¿Estas Seguro que quieres salir?", "Medidas",
+                MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+
+            if (r == DialogResult.Yes){
+                this.Close();
+            }
+                
+        }
+    }
+}
